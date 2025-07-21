bf7eb878a63081d5a0f5df65ff620ca89e3dc8f0
ARRA SRIHARSHA <22cs01043@iitbbs.ac.in>
2025-07-21 20:13:38 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -11,23 +11,31 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = [
-  'https://www.airfetch.online',
-  'http://localhost:5173'
-];
+const allowedOrigins = [ 
+    'https://www.airfetch.online',
+    'https://airfetch.online',  // Production frontend
+    'https://api.airfetch.online', // Production API
+    'http://localhost:5173', // Development frontend
+  ];
+  console.log(allowedOrigins);
+  app.use(cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like curl or Postman)
+      if (!origin) return callback(null, true);
+  
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        // Origin is allowed
+        callback(null, true);
+      } else {
+        // Origin is not allowed
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
 
-app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (like mobile apps, curl, etc.)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
